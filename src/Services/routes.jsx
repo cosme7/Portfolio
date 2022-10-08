c85bc03886b490b786d7268da05b8dcfffc8fe52
cosme7 <cosmewr@gmail.com>
2022-10-08 07:34:54 -0300

    Scroll to Top

--- src/Services/routes.jsx
+++ src/Services/routes.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {About, Contact, Footer, Header, Home, NotFound, Projects} from "../Pages"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 
 export default function routes(){
     return(
         <>
           <Router>
-              <Header />  
+              <Header />
+              <ScrollToTop />  
               <Routes>
                  <Route path='/' element={<Home />} />
                  <Route path='/projects' element={<Projects />} />
