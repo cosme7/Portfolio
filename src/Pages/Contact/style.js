594782bd4e3ec426b8ad208c59161c3601edb6c2
cosme7 <cosmewr@gmail.com>
2022-10-21 16:06:59 -0300

    More Tablet Corrections

--- src/Pages/Contact/style.js
+++ src/Pages/Contact/style.js
@@ -2,10 +2,12 @@ import styled from "styled-components";
 
 export const Container = styled.section`
     width: 100%;
+    min-height: 80vh;
     padding: var(--spacer-md);
     margin-inline: auto;
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
     row-gap: 1rem;
     background-color: var(--bg-two);
